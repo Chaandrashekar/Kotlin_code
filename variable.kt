ba5be7ade01ca07c2ed88818fa9db67f4c8ebcfd
Chandrashekar N <83531748+Chaandrashekar@users.noreply.github.com>
2022-07-12 12:29:10 +0530

    Create variable.kt

---
+++ variable.kt
@@ -0,0 +1,8 @@
+fun main(){ // main function//
+  var tim:String="Tim" /* var tim:String
+                        tim="Tim"
+                        println(tim)*/
+  println(tim) // printing tim variable//
+  val u:Int=3 // decalring numeric variable //
+  println(u) // printing the variable //
+}
