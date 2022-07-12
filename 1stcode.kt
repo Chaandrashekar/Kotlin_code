43e64385e748a457e53be4a3b878a436319bec11
Chandrashekar N <83531748+Chaandrashekar@users.noreply.github.com>
2022-07-12 12:20:19 +0530

    Update 1stcode.kt

--- 1stcode.kt
+++ 1stcode.kt
@@ -1,4 +1,4 @@
 // Write a program to print hello world in kotlin //
-fun main(args:Array<String>){  // main function which is used to execute in kotlin //
+fun main(){  // main function which is used to execute in kotlin //
 println("hello world")  // statement for execution//
 }
