bb2831f2750a15d3cbd51aa439a0980b93ce28e9
Chandrashekar N <83531748+Chaandrashekar@users.noreply.github.com>
2022-07-12 11:56:23 +0530

    Add files via upload

---
+++ 1stcode.kt
@@ -0,0 +1,4 @@
+// Write a program to print hello world in kotlin //
+fun main(args:Array<String>){  // main function which is used to execute in kotlin //
+println("hello world")  // statement for execution//
+}
